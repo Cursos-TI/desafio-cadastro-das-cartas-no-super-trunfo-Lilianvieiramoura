c77fcc413185f9466097687e05db289e5de293ad
Lílian Vieira <lilianv528@gmail.com>
2025-08-06 17:58:47 -0300

    ...

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -68,7 +68,7 @@ int main()
     printf("Estado: %s \n", estado1);
     printf("Código: %s \n", codigo1);
     printf("Nome da Cidade:  %s \n", cidade1);
-    printf("População: %lu\n", populacao1);
+    printf("População: %lu\\n", populacao1);
     printf("Área: %.2f km² \n", area1);
     printf("PIB: %.2f bilhões de reais \n", pib1);
     printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos1);
@@ -106,7 +106,7 @@ int main()
     printf("Estado: %s \n", estado2);
     printf("Código: %s \n", codigo2);
     printf("Nome da Cidade:  %s \n", cidade2);
-    printf("População: %lu\n", populacao2);
+    printf("População: %lu\\n", populacao2);
     printf("Área: %.2f km² \n", area2);
     printf("PIB: %.2f bilhões de reais \n", pib2);
     printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos2);
