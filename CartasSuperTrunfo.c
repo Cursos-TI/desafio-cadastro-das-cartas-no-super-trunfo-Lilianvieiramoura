3f6b4bcc366085355100cfc1481e497ed6e182d1
Lílian Vieira <lilianv528@gmail.com>
2025-08-06 17:43:58 -0300

    feat: melhorias sugerida pelo feedback

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 // initial commit
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -18,83 +19,91 @@ int main()
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    char estado[3];
-    char codigo[5];
-    char cidade[50];
-    int populacao;
-    float area;
-    float pib;
-    int quantidadePontosTuristicos;
+    char estado1[5];
+    char codigo1[5];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int quantidadePontosTuristicos1;
+
+    char estado2[5];
+    char codigo2[5];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int quantidadePontosTuristicos2;
 
     printf("Carta 1\n");
     printf("Digite o Estado: \n");
-    scanf("%s", estado);
+    scanf("%2s", estado1);
 
     printf("Digite o código da carta: \n");
-    scanf("%s", codigo);
+    scanf("%4s", codigo1);
 
     getchar(); // consome o \n deixado pelo scanf, sem usar while.
 
     printf("Digite o nome da cidade: \n");
-    fgets(cidade, sizeof(cidade), stdin);
-    cidade[strcspn(cidade, "\n")] = '\0'; // remove o '\n' do final
+    fgets(cidade1, sizeof(cidade1), stdin);
+    cidade1[strcspn(cidade1, "\n")] = '\0'; // remove o '\n' do final
 
-    printf("Digite a população: \n");
-    scanf("%d", &populacao);
+    printf("Digite a população: %lu\\n");
+    scanf("%lu", &populacao1);
 
     printf("Digite a área(em km2): \n");
-    scanf("%f", &area);
+    scanf("%f", &area1);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib);
+    scanf("%f", &pib1);
 
     printf("Digite a quantidade de pontos turísticos: \n");
-    scanf("%d", &quantidadePontosTuristicos);
+    scanf("%d", &quantidadePontosTuristicos1);
 
     printf("\n");
     printf("\nCarta 1: \n");
-    printf("Estado: %s \n", estado);
-    printf("Código: %s \n", codigo);
-    printf("Nome da Cidade:  %s \n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km² \n", area);
-    printf("PIB: %.2f bilhões de reais \n", pib);
-    printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos);
+    printf("Estado: %s \n", estado1);
+    printf("Código: %s \n", codigo1);
+    printf("Nome da Cidade:  %s \n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km² \n", area1);
+    printf("PIB: %.2f bilhões de reais \n", pib1);
+    printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos1);
 
     printf("\nCarta 2\n");
     printf("Digite o Estado: \n");
-    scanf("%s", estado);
+    scanf("%2s", estado2);
 
     printf("Digite o código da carta: \n");
-    scanf("%s", codigo);
+    scanf("%4s", codigo2);
 
     getchar(); // consome o \n deixado pelo scanf, sem usar while.
 
     printf("Digite o nome da cidade: \n");
-    fgets(cidade, sizeof(cidade), stdin);
-    cidade[strcspn(cidade, "\n")] = '\0'; // remove o '\n' do final
+    fgets(cidade2, sizeof(cidade2), stdin);
+    cidade2[strcspn(cidade2, "\n")] = '\0'; // remove o '\n' do final
 
     printf("Digite a população: \n");
-    scanf("%d", &populacao);
+    scanf("%d", &populacao2);
 
     printf("Digite a área(em km2): \n");
-    scanf("%f", &area);
+    scanf("%f", &area2);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib);
+    scanf("%f", &pib2);
 
     printf("Digite a quantidade de pontos turísticos: \n");
-    scanf("%d", &quantidadePontosTuristicos);
+    scanf("%d", &quantidadePontosTuristicos2);
 
     printf("\n");
     printf("\nCarta 2: \n");
-    printf("Estado: %s \n", estado);
-    printf("Código: %s \n", codigo);
-    printf("Nome da Cidade:  %s \n", cidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f km² \n", area);
-    printf("PIB: %.2f bilhões de reais \n", pib);
-    printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos);
+    printf("Estado: %s \n", estado2);
+    printf("Código: %s \n", codigo2);
+    printf("Nome da Cidade:  %s \n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km² \n", area2);
+    printf("PIB: %.2f bilhões de reais \n", pib2);
+    printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos2);
 
     return 0;
 }
