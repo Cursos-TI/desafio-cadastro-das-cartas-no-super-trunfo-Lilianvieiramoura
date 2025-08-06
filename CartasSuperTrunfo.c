221ac32f792fe61d096971ce8fad7d0d4196658d
Lílian Vieira <lilianv528@gmail.com>
2025-08-06 17:51:47 -0300

    mais melhorias

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,18 +19,18 @@ int main()
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    char estado1[5];
+    char estado1[1];
     char codigo1[5];
     char cidade1[50];
-    int populacao1;
+    unsigned long int populacao1;
     float area1;
     float pib1;
     int quantidadePontosTuristicos1;
 
-    char estado2[5];
+    char estado2[1];
     char codigo2[5];
     char cidade2[50];
-    int populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int quantidadePontosTuristicos2;
@@ -48,14 +48,17 @@ int main()
     fgets(cidade1, sizeof(cidade1), stdin);
     cidade1[strcspn(cidade1, "\n")] = '\0'; // remove o '\n' do final
 
-    printf("Digite a população: %lu\\n");
+    printf("Digite a população: \n");
     scanf("%lu", &populacao1);
+    getchar();
 
     printf("Digite a área(em km2): \n");
     scanf("%f", &area1);
+    getchar();
 
     printf("Digite o PIB: \n");
     scanf("%f", &pib1);
+    getchar();
 
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &quantidadePontosTuristicos1);
@@ -65,7 +68,7 @@ int main()
     printf("Estado: %s \n", estado1);
     printf("Código: %s \n", codigo1);
     printf("Nome da Cidade:  %s \n", cidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %lu\\n", populacao1);
     printf("Área: %.2f km² \n", area1);
     printf("PIB: %.2f bilhões de reais \n", pib1);
     printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos1);
@@ -84,13 +87,16 @@ int main()
     cidade2[strcspn(cidade2, "\n")] = '\0'; // remove o '\n' do final
 
     printf("Digite a população: \n");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
+    getchar();
 
     printf("Digite a área(em km2): \n");
     scanf("%f", &area2);
+    getchar();
 
     printf("Digite o PIB: \n");
     scanf("%f", &pib2);
+    getchar();
 
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &quantidadePontosTuristicos2);
@@ -100,7 +106,7 @@ int main()
     printf("Estado: %s \n", estado2);
     printf("Código: %s \n", codigo2);
     printf("Nome da Cidade:  %s \n", cidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %lu\\n", populacao2);
     printf("Área: %.2f km² \n", area2);
     printf("PIB: %.2f bilhões de reais \n", pib2);
     printf("Número de Pontos Turísticos: %d \n", quantidadePontosTuristicos2);
